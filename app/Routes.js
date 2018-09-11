1102a1a59a2a94184f8aa8b0d1391ca8cc3bc573
Richard Umoffia <richyafro@gmail.com>
2018-09-11 13:07:56 +0100

    Update routes list with new routes

--- app/Routes.js
+++ app/Routes.js
@@ -1,16 +1,18 @@
-/* eslint flowtype-errors/show-errors: 0 */
 import React from 'react';
-import { Switch, Route } from 'react-router';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+
 import routes from './constants/routes.json';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+import PackagePage from './containers/PackagePage';
 
 export default () => (
   <App>
-    <Switch>
-      <Route path={routes.COUNTER} component={CounterPage} />
-      <Route path={routes.HOME} component={HomePage} />
-    </Switch>
+    <Router>
+      <Switch>
+        <Route path="/package" component={PackagePage} />
+        <Route path={routes.HOME} component={HomePage} />
+      </Switch>
+    </Router>
   </App>
 );
