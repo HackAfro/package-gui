7c9d6a527c0fa83b996ea04d07c61f76c3693709
Richard Umoffia <richyafro@gmail.com>
2018-09-11 13:10:08 +0100

    Update window size

--- app/main.dev.js
+++ app/main.dev.js
@@ -62,8 +62,8 @@ app.on('ready', async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728
+    width: 700,
+    height: 550
   });
 
   mainWindow.loadURL(`file://${__dirname}/app.html`);
