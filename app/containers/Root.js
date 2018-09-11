d29497dfcd991efc899f465ca406c73fd2b47961
Richard Umoffia <richyafro@gmail.com>
2018-09-11 13:17:47 +0100

    Root component

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,24 +1,12 @@
-// @flow
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
-import type { Store } from '../reducers/types';
+import React, { Component, Fragment } from 'react';
 import Routes from '../Routes';
 
-type Props = {
-  store: Store,
-  history: {}
-};
-
-export default class Root extends Component<Props> {
+export default class Root extends Component {
   render() {
-    const { store, history } = this.props;
     return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <Routes />
-        </ConnectedRouter>
-      </Provider>
+      <Fragment>
+        <Routes />
+      </Fragment>
     );
   }
 }
