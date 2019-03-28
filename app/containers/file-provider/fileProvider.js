1c359c7374fd12b56ceb0a4ac36d93636a3e20fc
Richard Afro <richyafro@hotmail.com>
2019-03-28 21:03:44 +0100

    Update file provider to manage multiple files

--- app/containers/file-provider/fileProvider.js
+++ app/containers/file-provider/fileProvider.js
@@ -1,36 +1,74 @@
 import React, { createContext } from 'react';
 import PropTypes from 'prop-types';
 
-export const FileContext = createContext({});
+export const FileContext = createContext({
+  packagePaths: [],
+  currentPackagePath: '',
+  packageFolders: [],
+  currentPackageFolde: '',
+  setFilePath: () => {}
+});
 
 class FileProvider extends React.Component {
   constructor() {
     super();
     this.setFilePath = this.setFilePath.bind(this);
+    this.updateCurrentPackagePath = this.updateCurrentPackagePath.bind(this);
   }
 
   state = {
-    packagePath: '',
-    packageFolder: ''
+    packagePaths: [],
+    packageFolders: [],
+    currentPackageFolder: '',
+    currentPackagePath: ''
   };
 
   setFilePath({ packagePath, packageFolder }) {
     return new Promise((resolve, reject) => {
       try {
-        this.setState({ packagePath, packageFolder }, resolve);
+        this.setState(
+          prevState => ({
+            packagePaths: [packagePath, ...prevState.packagePaths],
+            packageFolders: [packagePath, ...prevState.packagePaths],
+            currentPackageFolder: packageFolder,
+            currentPackagePath: packagePath
+          }),
+          resolve
+        );
       } catch (e) {
         reject(e);
       }
     });
   }
 
+  updateCurrentPackagePath(packagePath, callback) {
+    this.setState(
+      {
+        currentPackagePath: packagePath
+      },
+      callback
+    );
+  }
+
   render() {
-    const { packagePath, packageFolder } = this.state;
+    const {
+      packagePaths,
+      currentPackagePath,
+      packageFolders,
+      currentPackageFolder
+    } = this.state;
     const { children } = this.props;
 
     return (
       <FileContext.Provider
-        value={{ packagePath, packageFolder, setFilePath: this.setFilePath }}
+        value={{
+          packagePaths,
+          currentPackagePath,
+          packageFolders,
+          currentPackageFolder,
+          setFilePath: this.setFilePath,
+          updateCurrentPackagePath: this.updateCurrentPackagePath
+        }}
       >
         {children}
       </FileContext.Provider>
