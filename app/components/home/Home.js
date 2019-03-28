906547b6f53cb6bc336d77a25a06910166729732
Richard Afro <richyafro@hotmail.com>
2019-03-28 21:00:10 +0100

    Update imports

--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
 import electron from 'electron';
+import PropTypes from 'prop-types';
 
-import { Redirect } from 'react-router-dom';
-import styles from './Home.css';
-import Button from '../button/button';
 import { FileContext } from '../../containers/file-provider/fileProvider';
+import Button from '../button/button';
+
+import styles from './Home.css';
+import AddFileIcon from './add-files.svg';
+import ProjectsList from './projectList/projectList';
 
 const { dialog } = electron.remote;
 
 export default class Home extends Component {
   static contextType = FileContext;
 
+  static propTypes = {
+    history: PropTypes.objectOf(PropTypes.any).isRequired
+  };
+
   constructor() {
     super();
     this.onButtonClick = this.onButtonClick.bind(this);
   }
 
-  state = { hasFile: false };
-
   onButtonClick(e) {
     const { setFilePath } = this.context;
-
+    const { history } = this.props;
     e.preventDefault();
 
     dialog.showOpenDialog(
@@ -36,19 +41,38 @@ export default class Home extends Component {
         const packageFolder = filePath.slice(0, packageIndex);
 
         await setFilePath({ packagePath: filePath, packageFolder });
-        this.setState({ hasFile: true });
+        history.push('/package');
       }
     );
   }
 
   render() {
-    const { hasFile } = this.state;
+    const { packagePaths } = this.context;
+    const { history } = this.props;
+    const containerStyle = packagePaths.length
+      ? styles.container
+      : styles.containerCenter;
 
-    return hasFile ? (
-      <Redirect to="/package" />
-    ) : (
-      <div className={styles.container} data-tid="container">
-        <Button onClick={this.onButtonClick}>Select Package file</Button>
+    return (
+      <div className={containerStyle} data-tid="container">
+        {packagePaths.length > 0 && (
+          <div className={styles.projectListContainer}>
+            <h3 className={styles.projectsHeader}>Opened Projects</h3>
+            <div className={styles.projectList}>
+              <ProjectsList projectPaths={packagePaths} history={history} />
+            </div>
+          </div>
+        )}
+        <div className={styles.fileSelectArea}>
+          <img
+            src={AddFileIcon}
+            className={styles.fileSelectIcon}
+            alt="add files"
+          />
+          <Button onClick={this.onButtonClick} primary>
+            <span>Select Package File</span>
+          </Button>
+        </div>
       </div>
     );
   }
